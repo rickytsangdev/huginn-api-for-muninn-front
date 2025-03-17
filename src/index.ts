2695bb938212e30080ef348f22e7dde7d063b9a4
rickytsangdev <rickytsang.dev@gmail.com>
2025-03-17 01:47:56 +0100

    add dev mode to build and restart server after a change
    install nodemon

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from processcoach server 🚀🔥");
+  res.send("Hello World from processcoach server c- modify test 2🚀🔥");
 });
 
 app.listen(port, () => {
