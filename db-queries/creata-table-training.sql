cf2aba411e19e2645f9dfc9710e26f5abe774be1
rickytsangdev <rickytsang.dev@gmail.com>
2025-03-19 21:59:48 +0100

    install sqlite3 database
    create first database training for test

---
+++ db-queries/creata-table-training.sql
@@ -0,0 +1,17 @@
+CREATE TABLE trainings
+
+(
+id INTERGER PRIMARY KEY, 
+title TEXT NOT NULL,
+description TEXT, 
+coach TEXT, 
+price INTEGER,
+picture TEXT, 
+location TEXT, 
+createdAt TEXT, 
+duration TEXT, 
+level TEXT, 
+format TEXT, 
+rating INTEGER 
+)
+
